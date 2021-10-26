bcea102ba9f0f19d092ff031605b8201aa85250f
mukhlisakbr <mukhlis3389@gmail.com>
2021-10-27 02:35:46 +0700

    feat: download working

--- src/components/ImagePreview.tsx
+++ src/components/ImagePreview.tsx
@@ -1,18 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 import { useImageContext } from "../context/Image";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Image, Flex } from "@chakra-ui/react";
 import Draggable from "react-draggable";
+import { useEffect, useRef } from "react";
+import useScreenshot from "../hooks/useScreenshot";
 
 export const ImagePreview = () => {
   const { image } = useImageContext();
+  const { generateImage, captureRef, status } = useScreenshot();
 
   return (
-    <Box position="relative" mb={8}>
-      <Draggable>
-        <div>AKU WM</div>
-      </Draggable>
-      <Box border="1px">
-        <img src={image} alt="" width="100%" />
+    <Box>
+      <Box boxShadow="2xl" rounded="3xl">
+        <Flex mb={8} justify="center">
+          <Flex position="relative" ref={captureRef}>
+            <Draggable>
+              <Box style={{ position: "absolute" }}>AKU WM</Box>
+            </Draggable>
+            <Image src={image} alt="KTP" />
+          </Flex>
+        </Flex>
+      </Box>
+      <Box>
+        <Button
+          disabled={status === "loading"}
+          colorScheme="blue"
+          onClick={generateImage}
+        >
+          Download
+        </Button>
       </Box>
     </Box>
   );
